a6150bc05e3b4108ea34fc23cd2abc89c58305e0
Chan Guan Hao <guanhao3797@gmail.com>
2016-07-25 03:15:22 +0800

    interface

--- index.js
+++ index.js
@@ -2,20 +2,39 @@
 import { NativeModules, DeviceEventEmitter } from 'react-native';
 
 const { SmartLocation } = NativeModules;
+
+type Coordinates = {
+  speed: number,
+  heading: number,
+  accuracy: number,
+  longitude: number,
+  altitude: number,
+  latitude: number,
+};
+
+type Position = {
+  timestamp: number,
+  timeTaken?: number, // self-added for measuring
+  coords: Coordinates,
+};
+
+
+function onLocationUpdate(cb: (pos: Position) => void) {
+  DeviceEventEmitter.addListener('smartLocationUpdated', (pos: Position) => {
+    cb(pos);
+  });
+  SmartLocation.onLocationUpdate();
+}
+
 const {
   getCurrentLocation,
   getLocationState,
   stopListeningForLocationUpdates,
 } = SmartLocation;
-export {
+
+module.exports = {
   getCurrentLocation,
   getLocationState,
   stopListeningForLocationUpdates,
+  onLocationUpdate,
 };
-
-export function onLocationUpdate(cb: (e: Event) => void) {
-  DeviceEventEmitter.addListener('smartLocationUpdated', (e: Event) => {
-    cb(e);
-  });
-  SmartLocation.onLocationUpdate();
-}
